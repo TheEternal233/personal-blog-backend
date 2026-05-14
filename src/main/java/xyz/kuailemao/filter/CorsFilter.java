a1d799521f2f85c4a47aaac41042616625522fc7
TheEternal <2950355750@qq.com>
2026-05-14 20:40:27 +0800

    修改中高危的安全问题

--- src/main/java/xyz/kuailemao/filter/CorsFilter.java
+++ src/main/java/xyz/kuailemao/filter/CorsFilter.java
@@ -4,12 +4,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import xyz.kuailemao.constants.Const;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author TheEternal
@@ -20,14 +22,23 @@ import java.io.IOException;
 @Order(Const.ORDER_CORS)
 public class CorsFilter extends OncePerRequestFilter {
 
+    @Value("#{'${cors.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain chain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        String origin = request.getHeader("Origin");
+
+        // 仅允许白名单中的 Origin
+        if (origin != null && allowedOrigins.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type,X-Client-Type,x-requested-with");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "3600");
 
         // 放行 OPTIONS 请求
